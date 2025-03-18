b9f447c0421a5b839696eec6eb6060373abc15a3
大国博之 <>
2025-03-18 15:16:46 +0900

    非同期処理見直し

--- index.js
+++ index.js
@@ -1,42 +1,55 @@
-window.addEventListener('load', function() {
-  liffInit()
+window.addEventListener('load', async function() {
+  const initRes = await liffInit()
+  console.log(initRes);
+  const profileData = await liffGetProfile()
+  console.log(profileData);
+  await setProfileData(profileData)
+  
 });
 
-const liffInit = ()=>{
-  alert('読み込み時');
-  console.log(window);
-  console.log(window.liff);
-  console.log(liff);
-  
-  
+const liffInit = async ()=>{
   // LIFF アプリの初期化
   liff.init({ liffId: '2007062380-kJ4LXXnz' })
-    .then(() => {
-      console.log('LIFF initialized successfully.');
-      alert('init成功');
+    .then((response) => {
       // 初期化後の処理（UI 表示の切り替えなど）を実装
+      return response
     })
     .catch((error) => {
-      console.error('Error initializing LIFF:', error);
-      alert('init失敗');
       // エラーハンドリング
+      return error
     });
     alert('init処理が流れてる');
-
+}
+const liffGetProfile = async ()=>{
   // ユーザー情報取得
   liff.getProfile()
     .then(profile => {
-      console.log('User Profile:', profile);
-      alert('User Profile:', profile);
-      const userId = profile.userId;
-      const displayName = profile.displayName;
-      const pictureUrl = profile.pictureUrl;
-      const statusMessage = profile.statusMessage;
-      
-      document.getElementById('user-info').innerText = 
-        `UserId: ${userId}\nDisplayName: ${displayName}\nStatus: ${statusMessage}`;
+      return profile
     })
     .catch(error => {
-      console.error('Error getting user profile:', error);
+      return error
     });
 }
+
+const setProfileData = async (profileData) => {
+  console.log(profileData);
+  alert(profileData)
+}
+// const liffGetProfile = ()=>{
+//   // ユーザー情報取得
+//   liff.getProfile()
+//     .then(profile => {
+//       console.log('User Profile:', profile);
+//       alert('User Profile:', profile);
+//       const userId = profile.userId;
+//       const displayName = profile.displayName;
+//       const pictureUrl = profile.pictureUrl;
+//       const statusMessage = profile.statusMessage;
+      
+//       document.getElementById('user-info').innerText = 
+//         `UserId: ${userId}\nDisplayName: ${displayName}\nStatus: ${statusMessage}`;
+//     })
+//     .catch(error => {
+//       console.error('Error getting user profile:', error);
+//     });
+// }
