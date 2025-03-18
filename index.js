39625451c69c69c69cd6b8050c3fe3c204a85870
大国博之 <>
2025-03-18 12:25:18 +0900

    検証用のalert表示

--- index.js
+++ index.js
@@ -1,19 +1,23 @@
 window.addEventListener('load', function() {
+  alert('読み込み時');
   // LIFF アプリの初期化
-  window.liff.init({ liffId: '2007062380-kJ4LXXnz' })
+  liff.init({ liffId: '2007062380-kJ4LXXnz' })
     .then(() => {
       console.log('LIFF initialized successfully.');
+      alert('init成功');
       // 初期化後の処理（UI 表示の切り替えなど）を実装
     })
     .catch((error) => {
       console.error('Error initializing LIFF:', error);
+      alert('init失敗');
       // エラーハンドリング
     });
 
   // ユーザー情報取得
-  window.liff.getProfile()
+  liff.getProfile()
     .then(profile => {
       console.log('User Profile:', profile);
+      alert('User Profile:', profile);
       const userId = profile.userId;
       const displayName = profile.displayName;
       const pictureUrl = profile.pictureUrl;
